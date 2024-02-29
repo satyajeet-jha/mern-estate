107c01df77802e234ad5497d549734d7eb2f3bb9
satyajeet-jha <72481692+satyajeet-jha@users.noreply.github.com>
2024-02-29 22:11:38 +0530

    Add files via upload

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArkjLssAuVAiVHQygdIhwowalwm0hXAiI",
+  authDomain: "mern-estate-419a8.firebaseapp.com",
+  projectId: "mern-estate-419a8",
+  storageBucket: "mern-estate-419a8.appspot.com",
+  messagingSenderId: "953655400581",
+  appId: "1:953655400581:web:11512813b68befdecc31ec"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signinStart: (state) => {
+            state.loading = true;
+        },
+        signinSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.error = null;
+            state.loading = false;
+        },
+        signinFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        updateUserStart: (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.error = null;
+            state.loading = false;
+        },
+        updateUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        signOutUserStart: (state) => {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        },
+        signOutUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+    },
+});
+
+export const { signinStart, signinSuccess, signinFailure, updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure,deleteUserStart,deleteUserSuccess, signOutUserFailure,signOutUserStart,signOutUserSuccess } = userSlice.actions;
+export default userSlice.reducer;
